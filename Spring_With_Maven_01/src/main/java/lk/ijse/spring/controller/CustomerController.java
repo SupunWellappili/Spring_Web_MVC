5bf03e3d2f1cb49571f29b4141ea49ea6a37518b
SupunWellappili <supunchandana1814@gmail.com>
2024-07-04 21:55:32 +0530

    DTO Created

--- Spring_With_Maven_01/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ Spring_With_Maven_01/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,14 +1,16 @@
 package lk.ijse.spring.controller;
 
 
+import lk.ijse.spring.dto.CustomerDTO;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class CustomerController {
-    public String  getAllcustomers(){
-        return "Hello Maven";
+    public CustomerDTO getAllcustomers(){
+        return new CustomerDTO("C001","IJSE","Galle",34000);
     }
 
 }
