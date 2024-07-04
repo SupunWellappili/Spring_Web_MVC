c91b591bd7cc674534a051f31499b1656f2ea9bf
SupunWellappili <supunchandana1814@gmail.com>
2024-07-04 22:09:12 +0530

    Jackson added

--- Spring_With_Maven_01/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ Spring_With_Maven_01/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -3,12 +3,15 @@ package lk.ijse.spring.controller;
 
 import lk.ijse.spring.dto.CustomerDTO;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping("/customer")
 public class CustomerController {
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public CustomerDTO getAllcustomers(){
         return new CustomerDTO("C001","IJSE","Galle",34000);
     }
