e6cc8c9c0502df1ec18619d6e209e2300dc2bee2
SupunWellappili <supunchandana1814@gmail.com>
2024-07-04 20:58:28 +0530

    Spring Web MVC Dependencies & Folder Structor Added

---
+++ Spring_With_Maven_01/src/main/java/lk/ijse/spring/WebAppInitializer.java
@@ -0,0 +1,24 @@
+package lk.ijse.spring;
+
+import lk.ijse.spring.config.WebAppConfig;
+import lk.ijse.spring.config.WebRootConfig;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Component
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
