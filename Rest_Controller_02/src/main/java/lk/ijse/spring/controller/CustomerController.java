c2155a06606bda5da96bf1520ad2e7a999675540
SupunWellappili <supunchandana1814@gmail.com>
2024-07-05 22:46:11 +0530

    Handler Methods/Mapping Methods & Hadler Mappings

--- Rest_Controller_02/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ Rest_Controller_02/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,9 +1,7 @@
 package lk.ijse.spring.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 //REST --> REpresentation State Transfer (A way of data transfer among client server apps)
@@ -14,6 +12,20 @@ public class CustomerController {
     @GetMapping
     public String getAllCustomer() {
         return "Hello there";
+    }
 
+    @PostMapping
+    public String saveCustomer(){
+        return "Customer Saved and Method Invoked!";
+    }
+
+    @PutMapping
+    public String updateCustomer(){
+        return "Customer Updated";
+    }
+
+    @DeleteMapping
+    public String deleteCustomer(){
+        return "Customer Delete";
     }
 }
