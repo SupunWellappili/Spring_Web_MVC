8a54f0c5b81b860c7ef70cde451dcdfdde34f255
SupunWellappili <supunchandana1814@gmail.com>
2024-07-05 17:17:06 +0530

    Controller Annotation with return & Unreturn

--- Spring_MVC_01/src/main/java/ik/ijse/spring/controller/ControllerOne.java
+++ Spring_MVC_01/src/main/java/ik/ijse/spring/controller/ControllerOne.java
@@ -1,54 +1,18 @@
 package ik.ijse.spring.controller;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.*;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/controllerOne")
-public class ControllerOne implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean
-{
-
-    public ControllerOne() {
-        System.out.println("Controller One Instantiated");
-    }
+public class ControllerOne {
 
     @GetMapping
     public void testMethod(){
         System.out.println("Request Receive");
     }
 
-    @Override
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        System.out.println("Spring Bean One Factory Aware");
 
-    }
-
-    @Override
-    public void setBeanName(String name) {
-        System.out.println("Spring Bean One Name Aware");
-
-    }
-
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("Spring Bean One Disposable Aware");
-
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("Spring Bean One Initializer ");
-
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println("Spring Bean One Application context Aware");
-
-    }
 }
--- Spring_MVC_01/src/main/java/ik/ijse/spring/controller/ControllerTwo.java
+++ Spring_MVC_01/src/main/java/ik/ijse/spring/controller/ControllerTwo.java
@@ -1,52 +1,20 @@
 package ik.ijse.spring.controller;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.*;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/two") //Handle Mapping
-public class ControllerTwo implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
 
-    public ControllerTwo() {
-        System.out.println("Controller Two Instantiated");
-    }
+@Controller
+@RequestMapping("/two") //Handle Mapping
+public class ControllerTwo  {
 
     @GetMapping //Mapping Methods/Handler Methods
     public String testTwo(){
         return "Hello Spring";
     }
 
-    @Override
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        System.out.println("Spring Bean Two Factory Aware");
-    }
 
-    @Override
-    public void setBeanName(String name) {
-        System.out.println("Spring Bean Two Name Aware");
-
-    }
-
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("Spring Bean Two Disposable ");
-
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("Spring Bean Two  Initializer");
-
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println("Spring Bean Two Application Context Aware");
-
-    }
 }
